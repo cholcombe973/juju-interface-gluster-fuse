df643dc2391fe34fd766e671c89c94027ec425e9
Chris Holcombe <xfactor973@gmail.com>
2017-02-13 13:52:14 -0800

    Swap out NFS for fuse relation

--- requires.py
+++ requires.py
@@ -4,7 +4,7 @@ from charms.reactive import hook, RelationBase, scopes
 class GlusterRequires(RelationBase):
     scope = scopes.UNIT
 
-    @hook('{requires:gluster-nfs}-relation-{joined,changed}')
+    @hook('{requires:gluster-fuse}-relation-{joined,changed}')
     def changed(self):
         conv = self.conversation()
         if conv.get_remote('gluster-public-address'):
@@ -12,12 +12,12 @@ class GlusterRequires(RelationBase):
             # it is part of the set of available units
             conv.set_state('{relation_name}.available')
 
-    @hook('{requires:gluster-nfs}-relation-{departed}')
+    @hook('{requires:gluster-fuse}-relation-{departed}')
     def departed(self):
         conv = self.conversation()
         conv.remove_state('{relation_name}.available')
 
-    @hook('{provides:gluster-nfs}-relation-{joined,changed}')
+    @hook('{provides:gluster-fuse}-relation-{joined,changed}')
     def changed(self):
         self.set_state('{relation_name}.connected')
 
